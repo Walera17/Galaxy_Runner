b995e238ffc5759c5eac6642e5a11a4bfd016528
DESKTOP-97ERRGR\Walera <aluschta17@gmail.com>
2022-12-18 06:04:42 +0200

    3. Capturing input swipes - Understanding how to capture and translate player input

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,14 +10,56 @@ public class Movement : MonoBehaviour
     private float verticalInput;
     private float horizontalTimer;
 
+    // Mobile Variables
+    private Vector2 startPosition;
+    private Vector2 deltaSwipe;
+
     void Update()
     {
         UpdatePersonalComputer();
-        UpdateMobile();
+        //UpdateMobile();
     }
 
     private void UpdateMobile()
     {
+        horizontalInput = verticalInput = 0;
+
+        if (Input.touches.Length > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+                startPosition = touch.position;
+            else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+                startPosition = deltaSwipe = Vector2.zero;
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                deltaSwipe = Vector2.zero;
+                if (startPosition != Vector2.zero)
+                    deltaSwipe = touch.position - startPosition;
+
+                if (deltaSwipe.sqrMagnitude > 5000) // >70^2 units
+                {
+                    float x = deltaSwipe.x;
+                    float y = deltaSwipe.y;
+
+                    if (MathF.Abs(x) >= MathF.Abs(y))
+                    {
+                        if (x > 0) horizontalInput = 1;
+                        else horizontalInput = -1;
+                    }
+                    else
+                    {
+                        if (y > 0) verticalInput = 1;
+                        else verticalInput = -1;
+                    }
+                    startPosition = deltaSwipe = Vector2.zero;
+                }
+            }
+        }
+
+        HorizontalMovement();
+        VerticalMovement();
     }
 
     private void UpdatePersonalComputer()
